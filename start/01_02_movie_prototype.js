225d74ec8831b3bc84370f21526785e4e0c0ee22
priyankadavle <priyankadavle@gmail.com>
2023-05-07 18:26:44 -0400

    Update 01_02_movie_prototype.js

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,16 @@
 // Write your code here
+// Write your code here
+class Movie{
+  constructor(title, director, genre, releaseYear, rating)
+  {
+    this.title = title;
+    this.director = director; this.genre= genre;
+    this.releaseYear=releaseYear; this.rating=rating;
+  }
+  getOverview(){
+    return `${this.title}, a ${this.genre} film directed by ${this.director}    in year ${this.releaseYear} with ating ${this.rating}`;
+  }
+
+}
+const movieInfo = new Movie("ABC", "director D", "simple", "1989", "***");
+console.log(movieInfo.getOverview());
